f0de2757c0e6d8c1638bdea39bfb62aab2677db1
FSMJB <luckyprogrammer24@gamil.comgit config --global user.name luckyprogrammer24>
2025-01-16 14:43:04 +0500

    inital commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Muhammad Junaid Portfolio
+
+Simple webpage crafted using pure HTML and CSS. A minimalistic showcase of structure and design.
+
+### Live Link: https://fsmjb.github.io/portfolio/
+
+## Features
+
+🎨 **Three Themes**: This project includes three distinct color themes, each with its own CSS file:
+
+1. **Blue Theme**
+2. **Red Theme**
+3. **Violet Theme**
+
+**_How to Switch Themes_** :
+
+To switch between these themes, link the desired theme's CSS file in your HTML. For example, to use the Red Theme:
---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(".footer__theme1").click(function(){
+$('html').css('--primary', '#12bdff');
+$('html').css('--primary-hover', '#0c8ec2');
+$('html').css('--dark-primary', '#1b1f24');
+});
+
+$(".footer__theme2").click(function(){
+$('html').css('--primary', '#ff5555');
+$('html').css('--primary-hover', '#e43232');
+$('html').css('--dark-primary', '#000');
+$('html').css('--dark-secondary', '#1e1b1b');
+$('html').css('--text-color', '#c4c4c4');
+});
+
+$(".footer__theme3").click(function(){
+$('html').css('--primary', '#ff29e4');
+$('html').css('--primary-hover', '#e01dc8');
+$('html').css('--dark-primary', '#4f1652');
+$('html').css('--dark-secondary', '#753980');
+$('html').css('--text-color', '#c4c4c4');
+});
