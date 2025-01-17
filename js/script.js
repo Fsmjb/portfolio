5ba504f1c9ff33dd2cc0da0f2da5d92a1af6d488
FSMJB <luckyprogrammer24@gamil.comgit config --global user.name luckyprogrammer24>
2025-01-17 08:39:09 +0500

    fix some style issue

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
+function theme1(){
+    $(':root').css('--primary', '#12bdff');
+    $(':root').css('--primary-hover', '#0c8ec2');
+    $(':root').css('--dark-primary', '#1b1f24');
+    $(':root').css('--dark-secondary', '#1e1b1b');
+    $(':root').css('--text-color', '#c4c4c4');
+};
+
+function theme2(){
+    $(':root').css('--primary', '#ff5555');
+    $(':root').css('--primary-hover', '#e43232');
+    $(':root').css('--dark-primary', '#000');
+    $(':root').css('--dark-secondary', '#1e1b1b');
+    $(':root').css('--text-color', '#c4c4c4');
+};
+
+function theme3(){
+    $(':root').css('--primary', '#ff29e4');
+    $(':root').css('--primary-hover', '#e01dc8');
+    $(':root').css('--dark-primary', '#4f1652');
+    $(':root').css('--dark-secondary', '#753980');
+    $(':root').css('--text-color', '#c4c4c4');
+};
+
 $(".footer__theme1").click(function(){
-$('html').css('--primary', '#12bdff');
-$('html').css('--primary-hover', '#0c8ec2');
-$('html').css('--dark-primary', '#1b1f24');
+    theme1();
 });
 
+
 $(".footer__theme2").click(function(){
-$('html').css('--primary', '#ff5555');
-$('html').css('--primary-hover', '#e43232');
-$('html').css('--dark-primary', '#000');
-$('html').css('--dark-secondary', '#1e1b1b');
-$('html').css('--text-color', '#c4c4c4');
+    theme2();
 });
 
 $(".footer__theme3").click(function(){
-$('html').css('--primary', '#ff29e4');
-$('html').css('--primary-hover', '#e01dc8');
-$('html').css('--dark-primary', '#4f1652');
-$('html').css('--dark-secondary', '#753980');
-$('html').css('--text-color', '#c4c4c4');
+    theme3();
 });
